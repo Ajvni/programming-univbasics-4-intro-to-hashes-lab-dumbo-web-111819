3c3fe1ad49b622042cc687fdc084404f10448997
Ajani Queeley <ajaniqueeley@gmail.com>
2019-11-13 23:13:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,12 +32,10 @@ end
 
 def update_counting_hash(hash, key)
   hash[key]
-  def update_hash(hash, key)
     if hash[key]
       hash[key] += 1
     else
       hash[key] = 1
     end
-  end
 
 end
